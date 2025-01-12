5a9573d53a9628247012b2da9203efb78608b7dd
tiendev2003 <tientc.21it@vku.udn.vn>
2025-01-12 09:11:00 +0700

    updaste

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const sequelize = require('./config/database');
-const authRoutes = require('./routes/authRoutes');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const sequelize = require("./config/database");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use('/api/auth', authRoutes);
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the application' });
+app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the application" });
 });
-
-sequelize.sync().then(() => {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on port 3000');
-  });
-}).catch(err => console.log(err));
+app.get("/tiendev", (req, res) => {
+  res.json({ message: "Welcome to the tiendev" });
+});
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => console.log(err));
